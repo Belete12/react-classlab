0aa6f9f5b3d4f211277ccadf3d2187ad903d7b45
Belete1117 <beleten143@gmail.com>
2025-08-29 05:07:12 -0400

    modifying the current structure like adding two new directory and move todolist, todolistitm, and todoform, and update app.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import TodoList from './TodoList'
-import TodoForm from './TodoForm'
+import TodoList from './features/TodoList/TodoList'
+import TodoForm from './features/TodoForm'
+
 
 function App() {
   const [todoList, setTodoList] = useState([]);
