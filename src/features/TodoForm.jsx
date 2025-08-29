b8489b4cc2519fe964408fa0185a6e3a1c430619
Belete1117 <beleten143@gmail.com>
2025-08-29 05:53:41 -0400

    adding the new TextInputWithLabel

--- src/features/TodoForm.jsx
+++ src/features/TodoForm.jsx
@@ -1,12 +1,14 @@
 //import React from 'react';
 import { useRef } from 'react';
 import {useState} from 'react';
+import TextInputWithLabel from '../shared/TextInputWithLabel';
+
 
 
 function TodoForm({onAddTodo}) {
     const [workingTodoTitle, setWorkingTodo] = useState('');
-
     const todoTitleInput= useRef(''); 
+
     function handleAddTodo(event){
         event.preventDefault();
         const title = workingTodoTitle.trim();
@@ -18,9 +20,20 @@ function TodoForm({onAddTodo}) {
   
       return (
     <form onSubmit={handleAddTodo}>
-          <label htmlFor="todoTitle">Todo</label>
-          <input type="title" id="todoTitle" name="title" value = {workingTodoTitle}
-          onChange={(event) => setWorkingTodo(event.target.value)}/>
+      <TextInputWithLabel         
+        ref={todoTitleInput}
+        value={workingTodoTitle}
+        onChange={(event) => setWorkingTodo(event.target.value)}
+        elementId ="todoTitle" 
+        labelText = "Todo"
+      />
+          {/* <label htmlFor="todoTitle">Todo</label>
+          <input 
+              type="title"
+              id="todoTitle" 
+              name="title" 
+              value = {workingTodoTitle}
+              onChange={(event) => setWorkingTodo(event.target.value)}/> */}
           <button type="submit" disabled={workingTodoTitle === ""}>Add Todo</button>
         </form>
       );
