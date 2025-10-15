45445df78674ab7207fc75bd4f51ee8cce0a49b1
Belete1117 <beleten143@gmail.com>
2025-10-15 01:16:45 -0400

    completed

--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -1,6 +1,7 @@
 import TodoListItem from './TodoListItem';
 import styles from './TodoList.module.css';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function TodoList({
   todoList,
@@ -10,36 +11,53 @@ function TodoList({
   isSaving,
 }) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+
   const itemsPerPage = 15;
   const currentPage = parseInt(searchParams.get('page') || '1', 10);
   const indexOfFirstTodo = (currentPage - 1) * itemsPerPage;
+
+  const filteredTodoList = todoList.filter((todo) => !todo.isCompleted);
+  const totalPages = Math.ceil(filteredTodoList.length / itemsPerPage);
+  const paginatedTodos = filteredTodoList.slice(
+    indexOfFirstTodo,
+    indexOfFirstTodo + itemsPerPage
+  );
+
+  useEffect(() => {
+    if (
+      (filteredTodoList.length > 0 && totalPages > 0 && isNaN(currentPage)) ||
+      currentPage < 1 ||
+      currentPage > totalPages
+    ) {
+      navigate('/');
+    }
+  }, [currentPage, totalPages, navigate, filteredTodoList.length]);
+
+  if (isLoading || isSaving) {
+    return <p>Todo list loading...</p>;
+  }
+
+  if (!filteredTodoList || filteredTodoList.length === 0) {
+    return <p>Add a todo above to get started</p>;
+  }
 
   function handlePreviousPage() {
     if (currentPage > 1) {
       setSearchParams({ page: currentPage - 1 });
     }
   }
+
   function handleNextPage() {
     if (currentPage < totalPages) {
       setSearchParams({ page: currentPage + 1 });
     }
   }
 
-  if (isLoading || isSaving) {
-    return <p>Todo list loading...</p>;
-  }
-
-  // Filter out completed todos
-  const filteredTodoList = todoList.filter((todo) => !todo.isCompleted);
-  if (!filteredTodoList || filteredTodoList.length === 0) {
-    return <p>Add a todo above to get started</p>;
-  }
-  const totalPages = Math.ceil(filteredTodoList.length / itemsPerPage);
-  // const paginatedTodos = filteredTodoList.slice(indexOfFirstTodo, indexOfFirstTodo + itemsPerPage);
   return (
     <>
       <ul className={styles.todoList}>
-        {filteredTodoList.map((todo) => (
+        {paginatedTodos.map((todo) => (
           <TodoListItem
             key={todo.id}
             todo={todo}
@@ -58,7 +76,9 @@ function TodoList({
         <span>
           Page {currentPage} of {totalPages}
         </span>
-        <button onClick={handleNextPage}>Next</button>
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </button>
       </div>
     </>
   );
